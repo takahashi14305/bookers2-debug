ac8d94b34cbd0d4e04d07e42a1b725c2baa1769e
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2022-12-06 12:32:37 +0000

    [finish] kadai7a

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
+  has_many :favorites_books, through: :favorites, source: :book
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
