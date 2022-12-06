5b4c7411f0da03d55240beea540393cee070293c
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2022-12-06 14:50:58 +0000

    [update] kadai8a

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
+  has_many :user_rooms
+  has_many :chats
+  has_many :rooms, through: :user_rooms
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
